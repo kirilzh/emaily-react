b49ef2cae80937f85d7081da47d683ecf7782a10
kirilzh <kiril.zhelyazkov9@gmail.com>
2018-07-27 22:08:20 +0300

    finished authentication with google

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 
+mongoose
+  .connect(keys.mongoURI)
+  .then(() => {
+    console.log('connected to mongo'); // if all is ok we will be here
+  })
+  .catch(err => {
+    // we will not be here...
+    console.error('App starting error:', err.stack);
+  });
+
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,20 @@
 const passport = require('passport');
+const mongoose = require('mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -11,9 +24,17 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken: ', accessToken);
-      console.log('refreshToken: ', refreshToken);
-      console.log('profile: ', profile);
+      User.findOne({ googleId: profile.id }).then(exitingUser => {
+        if (exitingUser) {
+          // we already have a record
+          done(null, exitingUser);
+        } else {
+          // we don't have a record with this ID
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
